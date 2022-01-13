bde40ba488b4fe2cad88dcc0b3671c8c08eb740a
Bryan <resendebryan@gmail.com>
2022-01-13 20:45:47 -0300

    Ideias iniciais

---
+++ BioPython.py
@@ -0,0 +1,5 @@
+# a ideia desse protjeto e usar o BioPython para acessar aos metadados das sequencias
+
+#A ideia é separar no documento as diferentes sequencias. Para isso, considerar o texto todo uma string
+# A partir dai, separar as strings de acordo com o texto //
+# com isso será possível procurar informações úteis de cada string (que será cada sequencia)
